b53dc25f6643fcda128f99fe5ff555fbc2a51ddd
Tswain <isaactom55@gmail.com>
2023-05-13 16:57:38 -0700

    matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A module for the 2D ritation function"""
+
+
+def rotate_2d_matrix(matrix):
+    """A function for rotating 2D matrix"""
+    holder = []
+    newlist = []
+
+    matrix.reverse()
+    if matrix is None:
+        return
+    for lists in matrix:
+        count = 0
+        for liss in lists:
+            if type(liss) == list:
+                return
+        if len(holder) == 0:
+            for liss in lists:
+                newlist = []
+                newlist.append(liss)
+                holder.append(newlist)
+        elif len(holder) != 0:
+            number = len(holder)
+            if count <= number:
+                for liss in lists:
+                    holder[count].append(liss)
+                    count += 1
+
+    matrix.clear()
+    for lists in holder:
+        matrix.append(lists)
